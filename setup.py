f113b9afdf4693f65ee719b3f9fd74575ad920bc
Raihat Zaman Neloy <raihat.zaman@newscred.com>
2018-01-24 16:30:38 +0600

    Add download url for the package

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
 	author = 'Raihat Zaman Neloy',
 	author_email = 'raihatneloy1992@gmail.com',
 	url = 'https://github.com/raihatneloy/timeout',
+	download_url = 'https://github.com/raihatneloy/timeout/archive/1.0.0.tar.gz',
 	keywords = ['timeout', 'codes-timeout'],
 	classifiers = []
 )
