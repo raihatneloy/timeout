b4045309b69020efe75f47d63140870a7058b0a0
Raihat Zaman Neloy <raihat.zaman@newscred.com>
2018-01-24 16:07:08 +0600

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+	name = 'timeout',
+	packages = ['timeout'],
+	version = '1.0.0',
+	description = 'Package for codes timeout',
+	author = 'Raihat Zaman Neloy',
+	author_email = 'raihatneloy1992@gmail.com',
+	url = 'https://github.com/raihatneloy/timeout',
+	keywords = ['timeout', 'codes-timeout'],
+	classifiers = []
+)
