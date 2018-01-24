aadc3a70a2eae7e6e1b275daaaa2df48cce16939
Raihat Zaman Neloy <raihat.zaman@newscred.com>
2018-01-24 16:56:35 +0600

    Update url in setup.py

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ setup(
 	description = 'Package for codes timeout',
 	author = 'Raihat Zaman Neloy',
 	author_email = 'raihatneloy1992@gmail.com',
-	url = 'https://github.com/raihatneloy/timeout',
-	download_url = 'https://github.com/raihatneloy/timeout/archive/1.0.0.tar.gz',
+	url = 'https://github.com/raihatneloy/timeout-checker',
+	download_url = 'https://github.com/raihatneloy/timeout-checker/archive/1.0.0.tar.gz',
 	keywords = ['timeout', 'codes-timeout'],
 	classifiers = []
 )
